7e48961e8b99558a1e1d3f989b223885bc8df9f0
Кирилл Цынгеев <ktsyngeev@bk.ru>
2024-05-19 13:42:45 +0300

    Добавил поиск пиццы

--- PizzaShop.WPF/VIewModel/PizzaViewModel.cs
+++ PizzaShop.WPF/VIewModel/PizzaViewModel.cs
@@ -4,9 +4,11 @@ using PizzaShop.WPF.Core;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace PizzaShop.WPF.VIewModel
 {
@@ -15,28 +17,50 @@ namespace PizzaShop.WPF.VIewModel
         private readonly IPizzaService _pizzaService;
 
         public ObservableCollection<PizzaDto> Pizzas { get; set; }
-
-        public string TextTest { get; set; }
+        public ICollectionView PizzaCollectionView { get; }
+        
+        private string _pizzaFilter = string.Empty;
+        public string PizzaFilter
+        {
+            get { return _pizzaFilter; }
+            set
+            {
+                _pizzaFilter = value;
+                OnPropertyChanged(nameof(PizzaFilter));
+                PizzaCollectionView.Refresh();
+            }
+        }
         public PizzaViewModel(IPizzaService pizzaService)
         {
-            _pizzaService = pizzaService;
 
+            _pizzaService = pizzaService;
             Pizzas = new ObservableCollection<PizzaDto>();
-            TextTest = "Хуесос";
+            PizzaCollectionView = CollectionViewSource.GetDefaultView(Pizzas);
+
+            PizzaCollectionView.Filter = FillterPizzas;
+            PizzaCollectionView.GroupDescriptions.Add(new PropertyGroupDescription(nameof(PizzaDto.Name)));
 
             LoadPizza();
+        }
 
+        private bool FillterPizzas(object obj)
+        {
+            if (obj is PizzaDto pizza)
+            {
+                return pizza.Name.Contains(PizzaFilter, StringComparison.InvariantCultureIgnoreCase);
+            }
+
+            return false;
+        }
+        public async Task LoadPizza()
+        {
+            Pizzas.Clear();
+            var adllPizzas = await _pizzaService.GetPizzaAllAsync();
 
             Pizzas.Add(new PizzaDto { Name = "Маргарита", Ingredients = new[] { "Соус", "Сыр", "Базилик" }, Size = "Средняя", Category = "Классические", Price = 250, Description = "Классическая и всегда популярная пицца с соусом, сыром и базиликом.", ImageSource = "/Images/Pizza1.jpg" });
             Pizzas.Add(new PizzaDto { Name = "Пепперони", Ingredients = new[] { "Соус", "Сыр", "Пепперони" }, Size = "Большая", Category = "Мясные", Price = 300, Description = "Сытная пицца с острой колбасой пепперони.", ImageSource = "/Images/Pizza2.jpg" });
             Pizzas.Add(new PizzaDto { Name = "Гавайская", Ingredients = new[] { "Соус", "Сыр", "Ветчина", "Ананасы" }, Size = "Средняя", Category = "Фруктовые", Price = 280, Description = "Нежная и сладкая пицца с ветчиной и ананасами.", ImageSource = "/Images/Pizza3.jpg" });
             Pizzas.Add(new PizzaDto { Name = "Новая", Ingredients = new[] { "Соус", "Сыр", "Ветчина", "Ананасы" }, Size = "Средняя", Category = "Фруктовые", Price = 280, Description = "Нежная и сладкая пицца с ветчиной и ананасами.", ImageSource = "/Images/Pizza4.jpg" });
-        }
-
-        public async Task LoadPizza()
-        {
-            Pizzas.Clear();
-            var adllPizzas = await _pizzaService.GetPizzaAllAsync();
 
             foreach (var income in adllPizzas)
             {
