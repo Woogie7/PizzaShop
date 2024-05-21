4751bbeb136b08b3a28e93fe259a9526dc1649b2
Кирилл Цынгеев <ktsyngeev@bk.ru>
2024-05-21 21:45:36 +0300

    Добавил валидацию для Регистрации

--- PizzaShop.WPF/VIewModel/RegisterViewModel.cs
+++ PizzaShop.WPF/VIewModel/RegisterViewModel.cs
@@ -5,16 +5,20 @@ using PizzaShop.WPF.Core;
 using PizzaShop.WPF.Service;
 using PizzaShop.WPF.Service.Store;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace PizzaShop.WPF.VIewModel
 {
-    internal class RegisterViewModel : ObservaleObject
+    internal class RegisterViewModel : ObservaleObject, INotifyDataErrorInfo
     {
+        private readonly ErrorValidationViewModel _errorValidation;
         private readonly IAuthenticator _authenticator;
         private CreateUserDto _user;
 
@@ -41,6 +45,9 @@ namespace PizzaShop.WPF.VIewModel
                 if (User.UserName != value)
                 {
                     User.UserName = value;
+
+                    ValidateName();
+
                     OnPropertyChanged();
                     OnPropertyChanged(nameof(CanRegister));
                 }
@@ -55,6 +62,9 @@ namespace PizzaShop.WPF.VIewModel
                 if (User.Email != value)
                 {
                     User.Email = value;
+
+                    ValidateEmail();
+
                     OnPropertyChanged();
                     OnPropertyChanged(nameof(CanRegister));
                 }
@@ -69,6 +79,9 @@ namespace PizzaShop.WPF.VIewModel
                 if (User.PasswordHash != value)
                 {
                     User.PasswordHash = value;
+
+                    ValidatePasswordHash();
+
                     OnPropertyChanged();
                     OnPropertyChanged(nameof(CanRegister));
                 }
@@ -92,6 +105,12 @@ namespace PizzaShop.WPF.VIewModel
             set => ErrorMessageViewModel.Message = value;
         }
 
+        public bool CanCreate => !HasErrors;
+
+        public bool HasErrors => _errorValidation.HasErrors;
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
         public RegisterViewModel(IAuthenticator authenticator, NavigationService<LoginViewModel> navigationServiceLogin)
         {
 
@@ -102,6 +121,62 @@ namespace PizzaShop.WPF.VIewModel
 
             RegisterCommand = new RegisterCommand(_authenticator, this);
             LoginNavigationCommand = new NavigateCommand<LoginViewModel>(navigationServiceLogin);
+
+            _errorValidation= new ErrorValidationViewModel();
+            _errorValidation.ErrorsChanged += ErrorsViewModel_ErrorsChanged;
+
+            ValidateEmail();
+            ValidatePasswordHash();
+        }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return _errorValidation.GetErrors(propertyName);
+        }
+
+        private void ErrorsViewModel_ErrorsChanged(object sender, DataErrorsChangedEventArgs e)
+        {
+            ErrorsChanged?.Invoke(this, e);
+            OnPropertyChanged(nameof(CanCreate));
+        }
+
+        private void ValidateEmail()
+        {
+            _errorValidation.ClearErrors(nameof(Email));
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                _errorValidation.AddError(nameof(Email), "Email обязательное поле!");
+            }
+            else if (!Regex.IsMatch(Email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                _errorValidation.AddError(nameof(Email), "Не корректный email.");
+            }
+        }
+
+        public void ValidatePasswordHash()
+        {
+            _errorValidation.ClearErrors(nameof(PasswordHash));
+            if (string.IsNullOrWhiteSpace(PasswordHash))
+            {
+                _errorValidation.AddError(nameof(PasswordHash), "Пароль обязательное поле!");
+            }
+            else if (PasswordHash.Length < 3)
+            {
+                _errorValidation.AddError(nameof(PasswordHash), "Пароль меньше 5 символов.");
+            }
+            else if (PasswordHash.Length > 15)
+            {
+                _errorValidation.AddError(nameof(PasswordHash), "Пароль больше 15 символов.");
+            }
+        }
+
+        public void ValidateName()
+        {
+            _errorValidation.ClearErrors(nameof(UserName));
+            if(string.IsNullOrEmpty(UserName))
+            {
+                _errorValidation.AddError(nameof(UserName), "Имя обязательное поле!");
+            }
         }
 
     }
